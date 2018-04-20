05f82370c852939e30aff90a4e4724c413baeaec
hjc <879404301@qq.com>
2018-04-20 13:36:58 +0800

    add finsh exam function

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from core import AutoMarx
 a = AutoMarx()
 result = a.auto_answer()
 if len(result) == 0:
-    print('全部成功 直接交卷吧!!无视他说只做了0题!!! 100了记得给我github点个星星啊!!! ')
+    print('全部提交成功')
 else:
     print("有" + str(len(result)) + "题提交失败")
-    for key in result.keys():
-        print(key + '答案为', end=' ')
-        for answer in result[key]:
-            print(chr(answer+65), end=' ')
-        print()
+
+a.finish_exam()
+print('已提交试卷 扫描二维码 查看信息')
